b501af63a0673571eb612d179dd23ec4cb086e14
nazo <nazo@nazo.dev>
2020-05-22 20:53:53 +0900

    ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,16 @@ export default () => (
               </div>
             </div>
           </div>
+          <form className="form bg-white p-6 my-10 relative" name="contact" method="POST" data-netlify="true">
+            <h3 className="text-2xl text-gray-900 font-semibold">お問い合わせ</h3>
+            <div className="flex space-x-5 mt-3">
+              <input type="text" name="" id="" placeholder="名前" className="border p-2  w-1/2" />
+              <input type="email" name="" id="" placeholder="メールアドレス" className="border p-2 w-1/2" />
+            </div>
+            <textarea name="" id="" cols="10" rows="10" placeholder="お問い合わせ内容" className="border p-2 mt-3 w-full"></textarea>
+            <input type="submit" value="送信" className="mt-6 bg-blue-600 hover:bg-blue-500 text-white font-semibold p-3" />
+          </form>
+          <div>このページのソースコードは <a href="https://github.com/nazo/nazo.dev" className="underline font-medium text-lg">https://github.com/nazo/nazo.dev</a> で公開されています。</div>
         </div>
       </div>
     </div>
