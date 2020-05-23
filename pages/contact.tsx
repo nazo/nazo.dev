c9ed9a17f5b26fc2123c95be2e4e72e24da9141e
nazo <nazo@nazo.dev>
2020-05-23 16:21:36 +0900

    form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,10 +11,10 @@ const PagesContact: React.FC = () => {
               <input type="hidden" name="form-name" value="contact" />
               <h3 className="text-2xl text-gray-900 font-semibold">お問い合わせ</h3>
               <div className="flex space-x-5 mt-3">
-                <input type="text" name="" id="" placeholder="名前" className="border p-2 w-1/2" />
-                <input type="email" name="" id="" placeholder="メールアドレス" className="border p-2 w-1/2" />
+                <input type="text" name="name" id="name" placeholder="名前" className="border p-2 w-1/2" />
+                <input type="email" name="email" id="email" placeholder="メールアドレス" className="border p-2 w-1/2" />
               </div>
-              <textarea name="" id="" cols={10} rows={10} placeholder="お問い合わせ内容" className="border p-2 mt-3 w-full"></textarea>
+              <textarea name="body" id="body" cols={10} rows={10} placeholder="お問い合わせ内容" className="border p-2 mt-3 w-full"></textarea>
               <input type="submit" value="送信" className="mt-6 bg-blue-600 hover:bg-blue-500 text-white font-semibold p-3" />
             </form>
           </div>
