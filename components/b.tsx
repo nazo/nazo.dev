50c364b21f3df56708b6b4b702c12a61f9378d13
nazo <nazo@nazo.dev>
2020-05-23 15:47:52 +0900

    b

--- components/b.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export const Profile: React.FC = () => (
-  <div className="md:w-1/3 p-4 text-sm">
-    <div className="text-3xl"><span className="font-medium">nazo</span></div>
-    <div className="my-2">フリーランスWebエンジニア</div>
-  </div>
-)
